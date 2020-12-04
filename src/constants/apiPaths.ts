ff5feea83e84abacd68cec76b76d32c562329afa
Evgeny Romanov (EPAM) <evgeny_romanov@epam.com>
2020-12-04 16:33:35 +0100

    http -> https

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://5h80lgbr6l.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://299bl3bv5g.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://evgeny-romanov-epam-cart-api-development.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://evgeny-romanov-epam-cart-api-development.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
